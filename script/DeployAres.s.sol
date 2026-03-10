fde9ccde048324e799e6af80cd3395322487e333
Theophilus <theophilusadesola002@gmail.com>
2026-03-10 22:50:49 +0100

    feat: Added logs to deploy script and tested adding participants via anvil chain

--- script/DeployAres.s.sol
+++ script/DeployAres.s.sol
@@ -1,7 +1,7 @@
-// SPDX-License-Identifier: MIT
+// // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 
 import {AresRegistry} from "../src/core/AresRegistry.sol";
 import {AresProposer} from "../src/modules/AresProposer.sol";
@@ -19,23 +19,35 @@ contract DeployAres is Script {
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
         address admin = vm.addr(deployerKey);
 
+        console.log("Deploying ARES Protocol with admin:", admin);
+        console.log("Chain ID:", block.chainid);
+
         vm.startBroadcast(deployerKey);
 
-        //Deploy
+        // Registry
         AresRegistry registry = new AresRegistry(admin);
+        console.log("AresRegistry deployed at:", address(registry));
 
-        //Deploy Modules
+        // Modules
         AresProposer proposer = new AresProposer(address(registry));
         AresSigner signer = new AresSigner(address(registry));
         AresTimelock timelock = new AresTimelock(address(registry));
         AresDistributor distributor = new AresDistributor(address(registry));
 
-        //Register Modules
+        // Register Modules in the Registry
         registry.registerModule(PROPOSER_MODULE, address(proposer));
         registry.registerModule(SIGNER_MODULE, address(signer));
         registry.registerModule(TIMELOCK_MODULE, address(timelock));
         registry.registerModule(DISTRIBUTOR_MODULE, address(distributor));
 
         vm.stopBroadcast();
+
+        console.log("-----------------------------------------");
+        console.log("ARES PROTOCOL DEPLOYMENT COMPLETE");
+        console.log("Proposer Module:   ", address(proposer));
+        console.log("Signer Module:     ", address(signer));
+        console.log("Timelock Module:   ", address(timelock));
+        console.log("Distributor Module:", address(distributor));
+        console.log("-----------------------------------------");
     }
 }
